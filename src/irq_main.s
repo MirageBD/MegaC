19ea29437475e004a0c2595613aa26ccd3855c77
MirageBD <firephoton@gmail.com>
2024-11-05 15:04:13 +0000

    Keep track of highest rastercount for player.

--- src/irq_main.s
+++ src/irq_main.s
@@ -16,6 +16,25 @@ irq_main:
 			lda #0x00
 			sta 0xd020
 
+			lda 0xd012
+			cmp highestraster
+			bmi irq_main_end
+			sta highestraster
+
+irq_main_end:
+			lda highestraster
+raswait:	cmp 0xd012
+			bne raswait
+
+			lda #0x02
+			sta 0xd020
+			ldx 0xd012
+			inx
+raswait2:	cpx 0xd012
+			bne raswait2
+			lda #0x00
+			sta 0xd020
+
 			plz
 			ply
 			plx
@@ -23,5 +42,7 @@ irq_main:
 			plp
 			asl 0xd019
 			rti
+
+highestraster	.byte 0x00
 
 ; ------------------------------------------------------------------------------------
